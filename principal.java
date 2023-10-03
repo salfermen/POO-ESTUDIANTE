c1bcd4128f2e476398ce82bdff4123f62b70beb3
Salim Ferez Mendez <123603908+salfermen@users.noreply.github.com>
2023-10-03 13:17:53 -0500

    Update principal.java

--- principal.java
+++ principal.java
@@ -19,8 +19,8 @@ public class Principal {
        estudiante.nota (sig1, sig2, sig3);
 
         System.out.println("Nombre del primer estudiante: " + name);
-        System.out.println("Fecha de nacimiento del primer estudiante: " +bornDate);
-        System.out.println("Promedio de notas del primer estudiante: " +estudiante );
+        System.out.println("Fecha de nacimiento del estudiante: " +bornDate);
+        System.out.println("Promedio de notas del estudiante: " +estudiante );
 
         scanner.close();
     }
